5d4e80088489f7c8d721122ed6308bd6a2564b53
Jose Guerra <josealaaguerra@gmail.com>
2022-02-12 12:58:41 -0600

    Agregue proyecto en c++

---
+++ cpp-bus-control/bcontrol.cpp
@@ -0,0 +1,27 @@
+class bcontrol
+{
+private:
+    /* data */
+public:
+    bcontrol(/* args */);
+    asignSeat();
+    freeSeat();
+    listSeats();
+    ~bcontrol();
+};
+
+bcontrol::bcontrol(/* args */)
+{
+}
+
+bcontrol::~bcontrol()
+{
+}
+bcontrol::asignSeat({
+}
+bcontrol::freeSeat(){
+}
+bcontrol::listSeats(){
+}
+
+
